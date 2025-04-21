62b8dbafbe04b47da74689197e32a56d1dcc25b5
Dilan Insaggi <dilaninsaggi@MacBook-Air-de-Dilan.local>
2025-04-21 12:12:20 -0400

    Primer commit

---
+++ src/main/java/com/dilaniz/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.dilaniz.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "¡HOLA MUNDO!";
+    }
+}
